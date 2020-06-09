19df47e4d3cb5a6540058d401ba5432fa96524e9
code-picasso <mail.shailesh.singh@gmail.com>
2020-06-09 19:51:38 +0530

    authorization service

---
+++ Code/AuthorizationService/src/main/java/com/cg/service/MyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.cg.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cg.model.MyUserDetails;
+import com.cg.model.User;
+import com.cg.repo.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	Logger logger = LoggerFactory.getLogger(MyUserDetailsService.class);
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUserName(username)
+				.orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
+		logger.info(user.toString());
+		return new MyUserDetails(user);
+	}
+
+}
